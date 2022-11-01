236eb9c4551da011959ae1f81298f3ab10688267
Yoganand Biradavolu <yoganandb2000@gmail.com>
2022-11-01 14:09:57 -0500

    Created Readme

---
+++ Tag/Hardware/AX5043_MSP430FR2355/AX5043 Radio Code/Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430fr2355.cmd 
+
+C_SRCS += \
+../ax5043.c \
+../main.c 
+
+C_DEPS += \
+./ax5043.d \
+./main.d 
+
+OBJS += \
+./ax5043.obj \
+./main.obj 
+
+OBJS__QUOTED += \
+"ax5043.obj" \
+"main.obj" 
+
+C_DEPS__QUOTED += \
+"ax5043.d" \
+"main.d" 
+
+C_SRCS__QUOTED += \
+"../ax5043.c" \
+"../main.c" 
+
+
